7acffe4580dcec4b08ba014623716b2f9173ed45
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-06 17:33:03 +0000

    refactoring project structure

--- trunk/s3games/src/s3games/io/GameSpecificationParser.java
+++ trunk/s3games/src/s3games/io/GameSpecificationParser.java
@@ -5,11 +5,19 @@
 
 package s3games.io;
 
+import s3games.engine.GameSpecification;
+
 /**
  *
  * @author petrovic16
  */
 public class GameSpecificationParser
 {
+    public GameSpecificationParser()
+    {
+    }
 
+    public void load(String fileName, GameSpecification specs)
+    {        
+    }
 }
