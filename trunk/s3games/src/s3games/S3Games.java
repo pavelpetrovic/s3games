ec27e3521570dd54b664c2462b88b12c4fd52044
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-06 14:01:22 +0000

    initial netbeans project

---
+++ trunk/s3games/src/s3games/S3Games.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package s3games;
+
+/**
+ *
+ * @author petrovic16
+ */
+public class S3Games {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
