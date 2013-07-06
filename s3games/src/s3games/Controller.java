1aae9e28c255aec29818aa6e213f077fe2153629
zuzycka <zuzycka@gmail.com>
2013-07-06 21:29:43 +0000

    ControllerWindow - file opening Controller - empty function readFile(BufferedReader r) added

--- s3games/src/s3games/Controller.java
+++ s3games/src/s3games/Controller.java
@@ -6,18 +6,37 @@
 package s3games;
 
 import s3games.gui.ControllerWindow;
+import java.util.*;
+import java.io.*;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author petrovic16
  */
-public class Controller
+public class Controller   
 {
+    ControllerWindow cw;
+    
     public Controller() {
         //GameWindow form =  new GameWindow();
         //form.setVisible(true);
-        ControllerWindow cw  = new ControllerWindow();
-        cw.setVisible(true);
-    
+        cw  = new ControllerWindow(this);
+        cw.setVisible(true);  
     }
+    
+    public boolean readFile(BufferedReader r) {  //or can return a number of players after parsing...
+        try {
+           //todo - use parser 
+           Scanner in=new Scanner(r);
+           while (in.hasNext()) {
+                System.out.println(in.next());
+           }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(cw, "bad file format");
+            return false;
+        }
+       return true;
+    }
+    
 }
