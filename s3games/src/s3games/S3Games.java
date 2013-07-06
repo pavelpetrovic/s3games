6e8dc76badace79e4940f36fdc35ac746d21aa24
zuzycka <zuzycka@gmail.com>
2013-07-06 16:50:01 +0000

    window created in controller, window editing..

--- s3games/src/s3games/S3Games.java
+++ s3games/src/s3games/S3Games.java
@@ -5,24 +5,21 @@
 
 package s3games;
 
-import s3games.gui.ControllerWindow;
-import s3games.gui.GameWindow;
 
 /**
  *
  * @author petrovic16
  */
 public class S3Games {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        GameWindow form =  new GameWindow();
-        form.setVisible(true);
-        ControllerWindow f  = new ControllerWindow();
-        f.setVisible(true);
+  
+        Controller controller = new Controller();
     }
 
 }
