40def27f14f330eda113819f536d5f78bb3d0e2e
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-06 17:13:20 +0000

    building further on the project structure

--- trunk/s3games/src/s3games/engine/Game.java
+++ trunk/s3games/src/s3games/engine/Game.java
@@ -5,11 +5,20 @@
 
 package s3games.engine;
 
+import io.GameLogger;
+import java.util.*;
+import player.Player;
+import s3games.gui.GameWindow;
+
 /**
  *
  * @author petrovic16
  */
 public class Game
 {
-
+    ExtendedGameState state;
+    Dictionary<String,Player> players;
+    GameLogger gameLog;
+    GameSpecification gameSpecification;
+    GameWindow window;
 }
