0003dd7ef6f17113058c7f159c05f9573f64aed5
zuzycka <zuzycka@gmail.com>
2013-07-07 13:15:36 +0000

    game win a board canvas elementary things

--- s3games/src/s3games/gui/BoardCanvas.java
+++ s3games/src/s3games/gui/BoardCanvas.java
@@ -5,19 +5,46 @@
 package s3games.gui;
 
 import java.awt.*;
+import s3games.engine.ExtendedGameState;
+import s3games.engine.GameSpecification;
 
 /**
  *
  * @author Boris
  */
 public class BoardCanvas extends Canvas {
+    GameSpecification gameSpec = null;
+    ExtendedGameState egameState = null;
+    Image bgImage;
     
     public BoardCanvas() {
       
     }
     
+    public void setGame(GameSpecification gs) {
+        gameSpec = gs;
+        bgImage = Toolkit.getDefaultToolkit().getImage(gs.boardBackgroundFileName);
+        egameState = null;
+        System.out.println("tu som");
+    }
+        
+    public void setState(ExtendedGameState egs) {
+        egameState = egs;
+        
+        
+        System.out.println("tuna");
+    }
+    
+    
     @Override
     public void paint(Graphics g) {
-        g.drawLine(4, 3, 50,48);
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());  //clear window
+        if (gameSpec!=null) {
+            g.drawImage(bgImage,0,0,this.getWidth(),this.getHeight(),this); //background
+            if (egameState!=null) {
+            
+            
+            }
+        }
     }
 }
