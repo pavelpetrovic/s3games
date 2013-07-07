cc95bf15b8edabfc6eb7bc062b851486f1e1c2f6
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-07 09:43:08 +0000

    controller vs. controllerWindow interface

--- trunk/s3games/src/s3games/Controller.java
+++ trunk/s3games/src/s3games/Controller.java
@@ -10,6 +10,7 @@ import java.util.*;
 import java.io.*;
 import javax.swing.JOptionPane;
 import s3games.io.*;
+import s3games.player.Player;
 
 /**
  *
@@ -20,6 +21,7 @@ public class Controller
     ControllerWindow cw;
     GameLogger logger;
     Config config;
+
     
     public Controller() {
         //GameWindow form =  new GameWindow();
@@ -30,6 +32,52 @@ public class Controller
         config = new Config(logger);
         config.load();
     }
+
+    //todo
+    public String[] getStrategiesForGame(String gameName)
+    {
+        return new String[] { "Random", "Intelligent123" };
+    }
+
+    //todo
+    public String[] getLearnableStrategyTypesForGame(String gameName)
+    {
+        return new String[] { "NN 7", "RL 5" };
+    }
+
+    //todo
+    public int getNumberOfPlayersForGame(String gameName)
+    {
+        return 1;
+    }
+
+    /** starts a single game
+     *
+     * @param gameName name of the game to play
+     * @param boardType real or simulated
+     * @param playerTypes for each player whether it is a human or a computer
+     * @param playerStrategies only for computer players, name of strategy to use
+     * @return the number of player who won, 0 for draw/nobody, -1 if game was interrupted
+     */
+    public int play(String gameName, Player.boardType boardType, Player.playerType[] playerTypes, String[] playerStrategies)
+    {
+        return 0;
+    }
+
+        /** starts a single game
+     *
+     * @param gameName name of the game to play
+     * @param boardType real or simulated
+     * @param playerTypes for each player whether it is a human or a computer
+     * @param playerStrategies only for computer players, name of strategy to use
+     * @param learnStrategy strategy that will be learned
+     * @return the number of player who won, 0 for draw/nobody, -1 if game was interrupted
+     */
+    public int learn(String gameName, Player.boardType boardType, Player.playerType[] playerTypes, String[] playerStrategies, String learnStrategyType, String strategyFileName, int numberOfRuns)
+    {
+        return 0;
+    }
+
     
     public boolean readFile(BufferedReader r)
     {  //or can return a number of players after parsing...
