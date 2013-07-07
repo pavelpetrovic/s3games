eec65151cdf53fcf67726fc1557af1e8c92bd948
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-07 12:06:38 +0000

    working on game specs parser

--- trunk/s3games/src/s3games/engine/Game.java
+++ trunk/s3games/src/s3games/engine/Game.java
@@ -9,6 +9,7 @@ import s3games.io.GameLogger;
 import java.util.*;
 import s3games.player.Player;
 import s3games.gui.GameWindow;
+import s3games.io.Config;
 
 /**
  *
@@ -18,7 +19,24 @@ public class Game
 {
     ExtendedGameState state;
     Dictionary<String,Player> players;
-    GameLogger gameLog;
+
     GameSpecification gameSpecification;
     GameWindow window;
+    
+    Config config;
+    GameLogger logger;
+
+    public Game(Config config, GameLogger logger)
+    {
+        this.config = config;
+        this.logger = logger;
+    }
+
+    public int play(String gameName, Player.boardType boardType, Player.playerType[] playerTypes, String[] playerStrategies)
+    {
+        gameSpecification = new GameSpecification(config, logger);
+        gameSpecification.load(gameName);
+
+        return 0;
+    }
 }
