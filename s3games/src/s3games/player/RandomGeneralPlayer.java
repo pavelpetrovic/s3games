f155827eca98dc6b7331c024c8bb682e54379311
pavel.petrovic <pavel.petrovic@gmail.com>
2013-07-14 09:42:55 +0000

    random player

--- s3games/src/s3games/player/RandomGeneralPlayer.java
+++ s3games/src/s3games/player/RandomGeneralPlayer.java
@@ -5,6 +5,7 @@
 package s3games.player;
 
 import java.util.ArrayList;
+import java.util.Random;
 import s3games.engine.GameSpecification;
 import s3games.engine.GameState;
 import s3games.engine.Move;
@@ -16,17 +17,19 @@ import s3games.engine.Move;
 public class RandomGeneralPlayer extends Player
 {
     GameSpecification specs;
+    Random rnd;
     
     public RandomGeneralPlayer(GameSpecification specs)
     {
         this.specs = specs;
+        rnd = new Random();
     }        
     
     @Override
     public Move move(GameState state, ArrayList<Move> allowedMoves) 
     {
-        
-        throw new UnsupportedOperationException("Not supported yet.");
+        try { Thread.sleep(1000); } catch (Exception e) {}
+        return allowedMoves.get(rnd.nextInt(allowedMoves.size()));        
     }
 
     @Override
