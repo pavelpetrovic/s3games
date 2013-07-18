5991d3db22700d9362b835e45c5fb4726ea42df6
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-18 05:10:17 +0000

    camera c++ project almost ready (remains to read game config file)

--- trunk/s3games/cameraBoard/cameraBoard/cameraBoard.cpp
+++ trunk/s3games/cameraBoard/cameraBoard/cameraBoard.cpp
@@ -7,19 +7,22 @@
 using namespace cv;
 using namespace std;
 
+inline int f2i(double f) { return (int) floor(f + 0.5); }
+
 class ElementType
 {
 private:
-	double hueMin, hueMax;
-	double satMin, satMax;
-	double valueMin, valueMax;
-	int sizeMin, sizeMax;
+	float hueMin, hueMax;
+	float satMin, satMax;
+	float valueMin, valueMax;
+	int sizeMin, sizeMax;	
 
 public:
 	char *elementTypeName;
 	int elementTypeState;
-
-	ElementType(double hueMin, double hueMax, double satMin, double satMax, double valueMin, double valueMax, int sizeMin, int sizeMax, char *elementTypeName, int elementTypeState)
+	int index;
+	
+	ElementType(float hueMin, float hueMax, float satMin, float satMax, float valueMin, float valueMax, int sizeMin, int sizeMax, char *elementTypeName, int elementTypeState, int index)
 	{
 		this->hueMin = hueMin;
 		this->hueMax = hueMax;
@@ -31,11 +34,15 @@ public:
 		this->sizeMax = sizeMax;
 		this->elementTypeName = elementTypeName;
 		this->elementTypeState = elementTypeState;
+		this->index = index;
 	}
 
 	bool matches(double hue, double sat, double value)
 	{		
-		return  (hue >= hueMin) && (hue <= hueMax) &&
+		bool hues = (hueMin < hueMax) ? ((hue >= hueMin) && (hue <= hueMax)) :
+			                            ((hue >= hueMin) || (hue <= hueMax));
+		if (!hues) return false;
+		return  
 				(sat >= satMin) && (sat <= satMax) &&
 				(value >= valueMin) && (value <= valueMax);
 	}
@@ -44,6 +51,95 @@ public:
 	{				
 		return (size >= sizeMin) && (size <= sizeMax);
 	}
+
+	void visualize(Vec3f &pt)
+	{
+		float avgHue = (hueMin < hueMax) ? ((hueMin + hueMax) / 2.0f) :
+			                               (f2i((hueMin + 360.0 + hueMax) / 2.0f) % 360);
+		pt[0] = avgHue;
+		pt[1] = ((satMin + satMax) / 2.0f);
+		pt[2] = ((valueMin + valueMax) / 2.0f);
+	}
+
+	char *toString()
+	{
+		static char str[200];
+		sprintf(str, "Hue: %d - %d, Sat: %.2f - %.2f, Val: %d - %d\n", f2i(hueMin), f2i(hueMax), satMin, satMax, f2i(valueMin), f2i(valueMax));
+		return str;
+	}
+
+	void decHueMin()
+	{
+		hueMin--;
+		if (hueMin < 0.0f) hueMin = 360.0f;
+	}
+
+	void incHueMin()
+	{
+		hueMin++;
+		if (hueMin > 360.0f) hueMin = 0.0f;
+	}
+
+	void decSatMin()
+	{
+		satMin -= 0.01f;
+		if (satMin < 0.0f) satMin = 0.0f;
+	}
+
+	void incSatMin()
+	{
+		satMin += 0.01f;
+		if (satMin > 1.0f) satMin = 1.0f;
+	}
+
+	void decValMin()
+	{
+		valueMin--;
+		if (valueMin < 0.0f) valueMin = 0.0f;
+	}
+
+	void incValMin()
+	{
+		valueMin++;
+		if (valueMin > 255.0f) valueMin = 255.0f;
+	}
+
+		void decHueMax()
+	{
+		hueMax--;
+		if (hueMax < 0.0f) hueMax = 360.0f;
+	}
+
+	void incHueMax()
+	{
+		hueMax++;
+		if (hueMax > 360.0f) hueMax = 0.0f;
+	}
+
+	void decSatMax()
+	{
+		satMax -= 0.01f;
+		if (satMax < 0.0f) satMax = 0.0f;
+	}
+
+	void incSatMax()
+	{
+		satMax += 0.01f;
+		if (satMax > 1.0f) satMax = 1.0f;
+	}
+
+	void decValMax()
+	{
+		valueMax--;
+		if (valueMax < 0.0f) valueMax = 0.0f;
+	}
+
+	void incValMax()
+	{
+		valueMax++;
+		if (valueMax > 255.0f) valueMax = 255.0f;
+	}
+
 };
 
 class Location 
@@ -67,9 +163,10 @@ class ElementMatcher
 
 public:
 	ElementMatcher(ElementType &etype, Mat &img, int x, int y) 
-		: findingImg(img), et(etype)
+		: fImg(img), et(etype)
 	{		
 		fsize = -1;
+		dir = -5;
 		fwidth = img.size().width;
 		fheight = img.size().height;
 		fx = x;
@@ -78,62 +175,95 @@ public:
 
 	long size()
 	{
-		if (fsize == -1)	
-			follow();		
+		if (fsize == -1)
+			if (follow()) getSize();
 		return fsize;
 	}
 
 private:	
-	Mat &findingImg;
+	Mat &fImg;
 	ElementType &et;
 	int fx, fy;
 	int	fwidth, fheight;
 	double fhue, fsat, fval;
-	int fsize;
+	long fsize;
+	int dir;
 
-	inline void getHSV()
+	void getHSV()
 	{
-		fhue = findingImg.at<Vec3f>(fy, fx)[0];
-		fsat = findingImg.at<Vec3f>(fy, fx)[1];
-		fval = findingImg.at<Vec3f>(fy, fx)[2];
+		fhue = fImg.at<Vec3f>(fy, fx)[0];
+		fsat = fImg.at<Vec3f>(fy, fx)[1];
+		fval = fImg.at<Vec3f>(fy, fx)[2];
 	}
 
-	inline void follow()
+	bool follow()
 	{
+		if (!goToNeighbor(dir)) return false;
 		getHSV();
-		if (et.matches(fhue, fsat, fval)) getSize();
+		if (et.matches(fhue, fsat, fval)) return true;
+		goToNeighbor(opposite(dir));
+		return false;
+	}
+
+	int opposite(int dir)
+	{
+		return (dir ^ 2);
+	}
+
+	int next(int dir)
+	{
+		return (dir % 4) - 1;
+	}
+
+	int previous(int dir)
+	{
+		return next(opposite(dir));
 	}
 
 	void getSize()
+	{	
+		dir = -1;
+		fImg.at<Vec3f>(fy, fx)[2] = (float)(-et.index);
+		do { step(); } while (findStep());
+	}
+
+	void step()
 	{		
-		findingImg.at<Vec3f>(fy, fx)[0] = -1;
-		findingImg.at<Vec3f>(fy, fx)[1] = -1;
-		findingImg.at<Vec3f>(fy, fx)[2] = -1;
+		fImg.at<Vec3f>(fy, fx)[1] = (float)opposite(dir);
+		dir = previous(dir);
+		fImg.at<Vec3f>(fy, fx)[0] = (float)dir; 
+		fImg.at<Vec3f>(fy, fx)[2] = (float)(-2 - et.index); 
 		fsize++;
-		if (fx > 0)
+	}
+
+	bool goToNeighbor(int dir)
+	{
+		switch (dir)
 		{
-			fx--;
-			follow();	
-			fx++;
+		case -1: if (fx > 0) fx--; else return false; break;
+		case -2: if (fy > 0) fy--; else return false; break;
+		case -3: if (fx < fwidth - 1) fx++; else return false; break;
+		case -4: if (fy < fheight - 1) fy++; else return false; break;
 		}
-		if (fy > 0)
+		return true;
+	}
+
+	bool findStep()
+	{		
+		while (!follow()) 
 		{
-			fy--;
-			follow();
-			fy++;
+			dir = next(dir);
+			fImg.at<Vec3f>(fy, fx)[0] = (float)dir;
+			while (dir == (f2i(fImg.at<Vec3f>(fy, fx)[1]))) 
+			{
+				if (fImg.at<Vec3f>(fy, fx)[2] < 0.0) return false;
+				goToNeighbor(dir);				
+				dir = f2i(fImg.at<Vec3f>(fy, fx)[0]);
+				dir = next(dir);
+				fImg.at<Vec3f>(fy, fx)[0] = (float)dir;
+			}
 		}
-		if (fx < fwidth - 1)
-		{
-			fx++;
-			follow();
-			fx--;
-		}
-		if (fy < fheight - 1)
-		{
-			fy++;
-			follow();
-			fy--;
-		}
+		return true;
 	}
 };
 
@@ -175,6 +305,27 @@ static void onMouse( int event, int x, int y, int, void* mouse )
 	mouseData[2] = y;
 }
 
+void visualize(Mat &img, Mat &vis, vector<ElementType *> &ets)
+{
+	int width = img.size().width; 
+	int height = img.size().height;
+
+	for (int x = 0; x < width; x++)
+		for (int y = 0; y < height; y++)
+			if (img.at<Vec3f>(y, x)[2] < -1)
+			{
+				int i = -2 - (int)(img.at<Vec3f>(y, x)[2]);
+				ets.at(i)->visualize(img.at<Vec3f>(y, x));
+			}
+
+	cvtColor(img, vis, CV_HSV2BGR );
+	Mat vis2(width, height, CV_8UC3);
+	vis.convertTo(vis2, CV_8UC3);
+
+	namedWindow( "result", CV_WINDOW_AUTOSIZE );
+	imshow( "result", vis2 );
+}
+
 int main( int argc, char** argv )
 {
 	vector<Location *> locations;
@@ -184,9 +335,10 @@ int main( int argc, char** argv )
 	Mat image;
 
 	//ElementType t1(82.0, 167.0, 0.3, 1.0, 76.0, 230.0, 100, 5000, "green", 1);
-	ElementType t1(12.0, 347.0, 0.3, 1.0, 76.0, 230.0, 100, 5000, "green", 1);
-	ElementType t2(250.0, 330.0, 0.3, 0.5, 100.0, 255.0, 100, 5000, "red", 1);
+	//ElementType t1(12.0, 347.0, 0.3, 1.0, 76.0, 230.0, 100, 5000, "green", 1, 0);
+	ElementType t2(343.0, 19.0, 0.5, 1.0, 100.0, 255.0, 100, 5000, "red", 1, 0);
 
+	//elementTypes.push_back(&t1);
 	elementTypes.push_back(&t2);
 
 	if (!cap->isOpened())
@@ -212,6 +364,7 @@ int main( int argc, char** argv )
 	}
 	Mat image32(image.size().width, image.size().height, CV_32FC3);
 	Mat hsv32(image.size().width, image.size().height, CV_32FC3);
+	Mat vis(image.size().width, image.size().height, CV_32FC3);
 
 	char key;
 
@@ -230,12 +383,86 @@ int main( int argc, char** argv )
 		if (mouseClicked)
 		{
 			image.convertTo(image32, CV_32FC3);
-			cvtColor(image32, hsv32, CV_BGR2HSV);
+			cvtColor(image32, hsv32, CV_RGB2HSV);
 			cout << "hue=" << hsv32.at<cv::Vec3f>(mouseY,mouseX)[0] << " ";
 			cout << "sat=" << hsv32.at<cv::Vec3f>(mouseY,mouseX)[1] << " ";
 			cout << "val=" << hsv32.at<cv::Vec3f>(mouseY,mouseX)[2] << endl;
 			mouseClicked = false;
 			key = -1;
+		}
+
+		
+		if (key == 'h')
+		{
+			elementTypes.at(0)->decHueMin();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'j')
+		{
+			elementTypes.at(0)->decHueMax();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'H')
+		{
+			elementTypes.at(0)->incHueMin();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'J')
+		{
+			elementTypes.at(0)->incHueMax();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 's')
+		{
+			elementTypes.at(0)->decSatMin();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'd')
+		{
+			elementTypes.at(0)->decSatMax();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'S')
+		{
+			elementTypes.at(0)->incSatMin();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'D')
+		{
+			elementTypes.at(0)->incSatMax();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'v')
+		{
+			elementTypes.at(0)->decValMin();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'b')
+		{
+			elementTypes.at(0)->decValMax();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'V')
+		{
+			elementTypes.at(0)->incValMin();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
+		}
+		else if (key == 'B')
+		{
+			elementTypes.at(0)->incValMax();
+			cout << elementTypes.at(0)->toString();
+			key = ' ';
 		}
 
 		if (key == ' ')
@@ -244,15 +471,17 @@ int main( int argc, char** argv )
 			cvtColor(image32, hsv32, CV_BGR2HSV);
 			ObjectLocator ol(hsv32, elementTypes, locations);
 			ol.findObjects();
-			cout << "objects:" << endl;
-			for (vector<Location *>::iterator it = locations.begin(); it < locations.end(); it++)
-				cout << (*it)->elementType << "(" << (*it)->elementState << ") at " << (*it)->x << ", " << (*it)->y << endl;
+			//cout << "objects:" << endl;
+			//for (vector<Location *>::iterator it = locations.begin(); it < locations.end(); it++)
+			//	cout << (*it)->elementType << "(" << (*it)->elementState << ") at " << (*it)->x << ", " << (*it)->y << endl;
 			key = -1;
+			visualize(hsv32, vis, elementTypes);
 		}
 
-	} while ( key == -1);
+	} while (key == -1);
 	
 	delete cap;
 
     exit(0);
 }
+
