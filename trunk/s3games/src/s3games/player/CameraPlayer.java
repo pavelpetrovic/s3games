a080bf4c781ee9e78a6b6adbe6c2efc1e6699e7b
pavel.petrovic@gmail.com <pavel.petrovic@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-21 18:01:13 +0000

    to interface with camera

--- trunk/s3games/src/s3games/player/CameraPlayer.java
+++ trunk/s3games/src/s3games/player/CameraPlayer.java
@@ -4,35 +4,60 @@
  */
 package s3games.player;
 
+
 import java.util.ArrayList;
+import java.io.*;
+
 import s3games.engine.GameSpecification;
 import s3games.engine.GameState;
 import s3games.engine.Move;
+import s3games.gui.CameraPlayerWindow;
 
 /**
  *
  * @author petrovic
  */
-public class CameraPlayer extends Player
+public class CameraPlayer extends Player implements Runnable
 {
     GameSpecification specs;
+    CameraPlayerWindow win;
+    Thread commThread;
+
+    private static final String CAMERA_PROGRAM = "cameraBoard/cameraBoard/OpenCV Release/cameraBoard.exe";
     
     public CameraPlayer(GameSpecification specs)
     {
         this.specs = specs;
-    }        
+        win = new CameraPlayerWindow(this);
+        commThread = new Thread(this);
+    }
     
     @Override
-    public Move move(GameState state, ArrayList<Move> allowedMoves) 
+    public void run()
     {
-        
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            Process p = Runtime.getRuntime().exec(CAMERA_PROGRAM);
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            PrintWriter out = new PrintWriter(p.getOutputStream());
+            if (!in.readLine().equals("S:S3 Games Camera"))
+                throw new Exception("Camera program did not respond");
+        } catch (Exception e)
+        {
+            win.addMessage(e.toString());
+        }
+
+    }
+    
+    @Override
+    public Move move(GameState state, ArrayList<Move> allowedMoves)
+    {
+        return allowedMoves.get(0);
     }
 
     @Override
-    public void otherMoved(Move move, GameState newState) 
+    public void otherMoved(Move move, GameState newState)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
-    
+
 }
