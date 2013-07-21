c49d44f64d49c299153c3d7b30d2ac26ea14910a
zuzycka@gmail.com <zuzycka@gmail.com@4f4596d2-aa15-080b-5f66-27d12ef4eeac>
2013-07-21 16:48:23 +0000

    alquerque jumps 1/2 + Astar, heuristic class

---
+++ trunk/s3games/src/s3games/ai/AStarStrategy.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s3games.ai;
+
+import s3games.engine.Game;
+import s3games.engine.GameSpecification;
+import s3games.player.AStarPlayer;
+import s3games.player.Player;
+
+/**
+ *
+ * @author Zuzka
+ */
+public class AStarStrategy extends Strategy {
+
+    @Override
+    public Player getPlayer(GameSpecification specs) {
+       return new AStarPlayer(specs);
+    }
+
+    @Override
+    public void learn(Game game) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
